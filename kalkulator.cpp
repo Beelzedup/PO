aa0c7e87f29e4e2a6e89c6dfc5c4a74f83cea4e5
Beelzedup <275884@student.pwr.edu.pl>
2025-03-18 18:17:26 +0100

    Add files via upload

---
+++ kalkulator.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double mem;
+bool mem_used;
+double add(double a, double b) {
+    return a + b;
+}
+double sub(double a, double b) {
+    return a - b;
+}
+double tim(double a, double b) {
+    return a * b;
+}
+double div(double a, double b) {
+    if (b == 0) {
+        return "Nie dzielimy przez 0 ";
+    }
+    return a / b;
+}
+
+double mod(double a, double b) {
+    if (b == 0) {
+        return "Nie dzielimy przez 0 ";
+    }
+    return a - (a/b) * b;
+}
+void erase(){}
+void menu(string dzialanie){
+    cout << 
+    switch (znak)
+    {
+    case +:
+        add();
+        break;
+
+    case -:
+        sub();
+            break;
+
+    case *:
+        tim();
+        break;
+
+    case /:
+        div();
+        break;
+
+    case %:
+        mod();
+        break;
+
+    case M:
+
+        break;
+
+    default:
+        cout << "Niepoprawnie podane polecenie" << endl;
+        break;
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
