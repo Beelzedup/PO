eb664e6708830d8d9cb6ddccc71ad0d659ecd698
Beelzedup <275884@student.pwr.edu.pl>
2025-03-23 18:47:19 +0100

    Update kalkulator.cpp

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -1,74 +1,128 @@
 ﻿#include <iostream>
-#include <cmath>
 using namespace std;
 
-double mem;
-bool mem_used;
+double mem = 0; //zmienna obsługująca pamięć 
+bool mem_zajeta = false; // zmienna informująca czy pamięć jest zajęta
+
+//dodawanie
 double add(double a, double b) {
     return a + b;
 }
+
+// odejmowanie
 double sub(double a, double b) {
     return a - b;
 }
+
+//mnożenie
 double tim(double a, double b) {
     return a * b;
 }
+
+//dzielenie
 double div(double a, double b) {
     if (b == 0) {
-        return "Nie dzielimy przez 0 ";
+        cout << "Błąd: Nie dzielimy przez zero" << endl;
+        return 0;
     }
     return a / b;
 }
 
+//modulo
 double mod(double a, double b) {
     if (b == 0) {
-        return "Nie dzielimy przez 0 ";
+         cout << "Błąd: Nie dzielimy przez zero" << endl;
+        return 0;
     }
     return a - (a/b) * b;
 }
-void erase(){}
-void menu(string dzialanie){
-    cout << "Dodawania ,, + '' " << endl;
-    cout << "Odejmowanie ,, - '' " << endl;
-    cout << "Mnożenie ,, * '' " << endl;
-    cout << "Dzielenie ,, / '' " << endl;
-    cout << "Modulo ,, % '' " << endl;
+
+//stan pamięci
+void pokaz_mem() {
+    if (mem_zajeta) {
+        cout << "Wartosc zapisana w pamieci: " << mem << endl;
+    } 
+    else {
+        cout << "Pamiec jest pusta " << endl;
+    }
+}
+
+//czyszczenie pamięci
+void wyczysc_mem() {
+    mem = 0;
+    mem_zajeta = false;
+    cout << "Pamiec wyczyszczona" << endl;
+}
+
+//menu
+void menu(){
+    double a, b, wynik;
+    char znak;
+    string wybor;
+    
+    cout << "Dodawania  (+) " << endl;
+    cout << "Odejmowanie (-) " << endl;
+    cout << "Mnozenie (*) " << endl;
+    cout << "Dzielenie (/) " << endl;
+    cout << "Modulo (%) " << endl;
+    cout << "Pamiec (M) " << endl;
+    cout << "Wyczysc pamiec (C) " << endl;
+    cout << "Zamknij program (X) " << endl;
+    cin >> znak;
+
+    if (znak == 'M') {
+        pokaz_mem();
+        return;
+    } 
+    else if (znak == 'C') {
+        wyczysc_mem();
+        return;
+    }
+    else if (znak == 'X') {
+        cout << "Koniec programu." << endl;
+        exit(0);
+    }
+
+    // Wpisanie liczb
+    cout << "Podaj pierwsza liczbe (lub uzyj pamieci (M) aby skopiowac poprzedni wynik): ";
+    cin >> wybor;
+    if (wybor == "M" && mem_zajeta) a = mem; else a = stod(wybor); // zmiana tekstu string na liczbę double
+
+    cout << "Podaj druga liczbe (lub uzyj pamieci (M) aby skopiowac wynki): ";
+    cin >> wybor;
+    if (wybor == "M" && mem_zajeta) b = mem; else b= stod(wybor);
+    
     switch (znak)
     {
-    case +:
-        add();
+    case '+':
+        wynik = add(a, b);
         break;
-
-    case -:
-        sub();
+    case '-':
+        wynik = sub(a, b);
             break;
-
-    case *:
-        tim();
+    case '*':
+        wynik = tim(a, b);
         break;
-
-    case /:
-        div();
+    case '/':
+        wynik = div(a, b);
         break;
-
-    case %:
-        mod();
+    case '%':
+        wynik = mod(a, b);
         break;
-
-    case M:
-
-        break;
-
     default:
         cout << "Niepoprawnie podane polecenie" << endl;
-        break;
+        return;
     }
-}
-    }
+    cout << "Wynik: " << wynik << endl;
+    mem = wynik;
+    mem_zajeta = true;
+    cout << "Wynik zapisany do pamieci" << endl;
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    while (true) {
+        menu();
+    }
+return 0;
 }
 
