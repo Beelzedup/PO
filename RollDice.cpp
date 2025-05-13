3bb3f568c555b0336c55cdb40045f55812382742
Beelzedup <275884@student.pwr.edu.pl>
2025-05-13 17:14:31 +0200

    RollDice.cpp

--- RollDice.cpp
+++ RollDice.cpp
@@ -8,21 +8,25 @@ int rollDice(int sides) {
 
 int main() {
     std::srand(std::time(0));
-
+    
+    int d3 = rollDice(3);
     int d4 = rollDice(4);
     int d6 = rollDice(6);
     int d8 = rollDice(8);
     int d10 = rollDice(10);
     int d12 = rollDice(12);
     int d20 = rollDice(20);
-
+    int d100 = rollDice(100);
+    
     std::cout << "Wyniki rzutów kośćmi:" << std::endl;
+    std::cout << "d3: " << d3 << std::endl;
     std::cout << "d4: " << d4 << std::endl;
     std::cout << "d6: " << d6 << std::endl;
     std::cout << "d8: " << d8 << std::endl;
     std::cout << "d10: " << d10 << std::endl;
     std::cout << "d12: " << d12 << std::endl;
     std::cout << "d20: " << d20 << std::endl;
+    std::cout << "d100: " << d100 << std::endl;
 
     return 0;
 }
