5d90c41c105af3e5e14619371eb121c29c2a9c3b
Beelzedup <275884@student.pwr.edu.pl>
2025-05-27 16:23:01 +0200

    Delete RPG_PROJECT directory

--- RPG_PROJECT/FileManager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Character.h"
-#include <string>
-
-class FileManager {
-public:
-    void saveCharacter(const Character& character, const std::string& filename);
-    Character loadCharacter(const std::string& filename);
-};
